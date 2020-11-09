1508e3662f0ed5ab8ab6b0af6d48b306a94a6751
Thomas Kirst <thomas.kirst@neuefische.de>
2020-11-09 13:50:32 +0100

    Added files

---
+++ server.js
@@ -0,0 +1,29 @@
+const http = require('http');
+const filesystem = require('fs');
+
+const hostname = '127.0.0.1';
+const port = 4000;
+
+const server = http.createServer((req, res) => {
+  const { url, method } = req;
+
+  const routes = {
+    '/students': () => {
+      res.setHeader('Content-Type', 'application/json');
+      res.end(JSON.stringify(['Hendrik', 'Jana']));
+    },
+    404: () => {
+      res.setHeader('Content-Type', 'text/html');
+      const errorPage = filesystem.readFileSync('404.html');
+      res.end(errorPage);
+    },
+  };
+
+  // url = '/students'
+  // routes['/students'] = fn() -> "method invocation" / "call a function" ()
+  routes[url] ? routes[url]() : routes[404]();
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server started on port http://${hostname}:${port}`);
+});
